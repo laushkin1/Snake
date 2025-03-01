4053dcfa35c255e1dd5a7d77bea5bdeded6049ab
laushkin1 <laushkinivan1@gmail.com>
2025-03-01 16:02:36 +0100

    add settings.py

---
+++ game/settings.py
@@ -0,0 +1,11 @@
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 700
+
+FPS = 60
+WINDOW_NAME = "Snake"
+
+SPRITES = {
+        "snake_head": "game/sprites/snake_head.png",
+        "snake_body": "game/sprites/snake_body.png",
+}
+
